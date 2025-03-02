85434237485856688aed535907a2aa7218ef70c0
MeiirimZh <zh.meyirim@gmail.com>
2025-03-02 12:56:20 +0500

    added win conditions; added info text

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import keyboard
 import time
 import os
-from random import choice
+import random
 
 
 class Game:
@@ -9,11 +9,26 @@ class Game:
         self.player_wins = 0
         self.ai_wins = 0
 
+        self.player_choice = None
+        self.ai_choice = None
+
         self.choices = ["Rock", "Paper", "Scissors"]
         self.current_choice = 0
 
+        self.win = None
+        self.win_conditions = {"Rock": "Scissors", "Paper": "Rock", "Scissors": "Paper"}
+        self.win_text = {"Player": "Player won!\n", "AI": "Computer won!\n", "Draw": "Draw!\n"}
+
     def run(self):
         while True:
+            print(f'Player: {self.player_wins}\tComputer: {self.ai_wins}\n')
+
+            if self.player_choice and self.ai_choice:
+                print(f'Player: {self.player_choice}\nComputer: {self.ai_choice}')
+
+            if self.win:
+                print(self.win_text[self.win])
+
             for choice in self.choices:
                 if choice == self.choices[self.current_choice]:
                     print(f'> {choice}')
@@ -26,10 +41,25 @@ class Game:
                 self.current_choice = max(0, self.current_choice - 1)
             if key =="down":
                 self.current_choice = min(len(self.choices) - 1, self.current_choice + 1)
+            if key == "enter":
+                self.player_choice = self.choices[self.current_choice]
+                self.ai_choice = random.choice(self.choices)
+                self.win = self.check_win(self.ai_choice)
 
             time.sleep(.15)
 
             os.system("cls")
+
+    def check_win(self, ai_choice):
+        if self.choices[self.current_choice] == ai_choice:
+            return "Draw"
+        
+        if self.win_conditions[self.choices[self.current_choice]] == ai_choice:
+            self.player_wins += 1
+            return "Player"
+        else:
+            self.ai_wins += 1
+            return "AI"
 
 
 if __name__ == "__main__":
